3786663b11555f59b0020d8390a6604533348129
Nir Klieman <nirr53@gmail.com>
2018-09-13 12:22:56 +0300

    Endantion fixes
    Endantion fixes

--- src/xmlReader.java
+++ src/xmlReader.java
@@ -11,7 +11,7 @@ import org.w3c.dom.Node;
 
 public class xmlReader {
 
-   public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+   public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {   
 
        // Get input and normalize it
 	   File inputFile = new File("input.txt");     
@@ -26,33 +26,33 @@ public class xmlReader {
 
    @SuppressWarnings("static-access")
    private static void parse(Node parentNode, int block) {
-	
-	// Check current element kids number 
-    NodeList nList = parentNode.getChildNodes();
-    block++;
+	   
+	   // Check current element kids number     
+	   NodeList nList = parentNode.getChildNodes();
+	   block++;
     
-    // if the kids number is zero, return
-    int nodesNum = nList.getLength();  
-    if (nodesNum == 0) {
+	   // if the kids number is zero, return
+	   int nodesNum = nList.getLength();  
+	   if (nodesNum == 0) {
     	
-    	return;
-    }
+		   return;
+	   }
     
-    // Loop on all kids and print their name
-    for (int temp = 0; temp < nodesNum; temp++) {
-	 
-    	Node nNode = nList.item(temp);
-    	if (nNode.getNodeType() == nNode.ELEMENT_NODE) {
+	   // Loop on all kids and print their name 
+	   for (int temp = 0; temp < nodesNum; temp++) {
+    
+		   Node nNode = nList.item(temp);
+		   if (nNode.getNodeType() == nNode.ELEMENT_NODE) {
 
-    		System.out.println(getBlocks(block) + "Current Element :" + nNode.getNodeName());
-    		parse(nNode, block);
-    	}
-    }
+			   System.out.println(getBlocks(block) + "Current Element :" + nNode.getNodeName());
+			   parse(nNode, block);
+		   }
+	   }
    }
 
    // Just add tab for prints depend on level
    private static String getBlocks(int block) {
-	
+	   	
 	   String temp = "";
 	   for (int j = 0 ; j < block; ++j) {
 		   
